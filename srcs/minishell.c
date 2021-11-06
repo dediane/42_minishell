93a56a4d4665eade80f90d2e1d7952aad78366c2
Diane <dianedecourt@gmail.com>
2021-11-06 13:53:02 +0100

    prep of the github

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 19:28:36 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/10/26 19:49:38 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/11/06 13:52:43 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,5 @@
 
 int main(void)
 {
-	char	*buffer = NULL;
-	size_t	buf_size = 2048;
 
-	// alloc buffer qui stockera la commande entree par l'user
-	buffer = (char *)malloc(sizeof(char) * buf_size);
-	if (buffer == NULL) 
-	{
-		perror("Malloc failure");
-		return (EXIT_FAILURE);
-	}
-	
-	write(1, "$> ", 3);
-
-	// lecture de STDIN en boucle
-	while (getline(&buffer, &buf_size, stdin) > 0) {
-		printf("cmd = %s\n", buffer);
-		write(1, "$> ", 3);
-	}
-
-	printf("Bye \n");
-	free(buffer);
 }
