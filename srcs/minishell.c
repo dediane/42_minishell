8460d632b86bd114877566e15cedf2dc0f1d3808
dediane <ddecourt@student.42.fr>
2021-11-09 11:45:26 +0100

    update Makefile and test for prompt readline

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,15 +6,23 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 19:28:36 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/11/06 13:52:43 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/11/09 11:35:00 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-//void	ft_init_prompt()
-
 int main(void)
 {
+	//char *line = NULL;
 
+	while (1)
+	{
+		printf( "%s\n", readline( "Minishell$ " ) );
+		/*printf("minishell $ - ");
+		readline();
+		//get_next_line(1, &line);
+		printf("%s\n", line);*/
+	}
+	return (0);
 }
