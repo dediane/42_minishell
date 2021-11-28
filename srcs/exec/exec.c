45c9b172cecf7a5c2b4a4a5f0ec19f3ec7c3b6c7
dediane <dianedecourt@gmail.com>
2021-11-28 21:01:34 +0100

    Fixing simple redirection

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 12:03:55 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/11/28 18:45:44 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/11/28 21:00:24 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,29 +151,30 @@ int		ft_exec_all_cmd(t_parsing *params, char **envp)
 	fd = 0;
 	while (params->next)
 	{
-		//params->fd_stdin = dup(STDIN);
-		//params->fd_stdout = dup(STDOUT);
+		params->fd_stdin = dup(STDIN);
+		params->fd_stdout = dup(STDOUT);
 		if (params->type != 0)
 			fd = open_file(params, params->file->name);
-		//dup2(fd, 1);
-		//close(fd);
+		dup2(fd, 1);
 		ft_exec(params, envp);
-		//close(params->fd_stdin);
-		//close(params->fd_stdout);
+		close(fd);
+		close(params->fd_stdin);
+		dup2(params->fd_stdout, STDOUT);
 		if (params->next)
 			params = params->next;
 	}
-	//close (STDOUT_FILENO);
 	if (params->type != 0)
 	{
-		//params->fd_stdin = dup(STDIN);
-		//params->fd_stdout = dup(STDOUT);
+		params->fd_stdin = dup(STDIN);
+		params->fd_stdout = dup(STDOUT);
 		fd = open_file(params, params->file->name);
-		//dup2(fd, 1);
+		dup2(fd, 1);
+		ft_exec(params, envp);
+		close(fd);
+		close(params->fd_stdin);
+		dup2(params->fd_stdout, STDOUT);
 	}
-	ft_exec(params, envp);
-	//close(fd);
-	//close(params->fd_stdin);
-	//close(params->fd_stdout);
+	else
+		ft_exec(params, envp);
 	return (0);
 }
