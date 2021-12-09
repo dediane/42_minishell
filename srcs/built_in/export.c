92d28d17a9a07b30a39b3f9f14ec887c592c7713
dediane <ddecourt@student.42.fr>
2021-12-09 21:56:47 +0100

    strt export still need to be finish

--- srcs/built_in/export.c
+++ srcs/built_in/export.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 14:55:05 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/12/09 19:54:28 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/12/09 21:54:06 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int	export(char *line, char **envp)
 {
-	int i;
-	int pos;
-	int size;
-	char *value;
+	int		i;
+	int		pos;
+	int		size;
+	char	*value;
 
 	i = 0;
 	size = 0;
-	while ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= '0' && line[i] <= '9'))
-	{
+	while ((line[i] >= 'a' && line[i] <= 'z') || \
+	(line[i] >= 'A' && line[i] <= 'Z') || (line[i] >= '0' && line[i] <= '9'))
 		i++;
-	}
 	if (line[i] != '=')
-		return(0);
+		return (0);
 	pos = i++;
 	while (line[i] != '\n')
 		i++;
 	size = i - pos;
+	return (0);
 }
